6da0526e4e16c1718ae265d106c4f9c8afb60cc3
Dehvay <dymg.ngeva@outlook.com>
2023-11-24 19:18:28 -0600

    funcion clonapalabras completada

--- Corrector.cpp
+++ Corrector.cpp
@@ -21,6 +21,7 @@ int esEspacioBlanco(char c) {
 	return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\0' || c == '.' || c == ',' || c == '(' || c == ')' || c == ';';
 }
 
+
 //Funciones publicas del proyecto
 /*****************************************************************************************************************
 	DICCIONARIO: Esta funcion crea el diccionario completo
@@ -34,7 +35,7 @@ void	Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[
 	FILE* fpDict;
 	char texto[4000];
 	char wdetec[200];
-	int cont, contp, iNumE;
+	int cont, contp, iNumE, ltexto, i;
 	iNumElementos = 0;
 
 	fopen_s(&fpDict, szNombre, "r");
@@ -46,15 +47,15 @@ void	Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[
 		while (!feof(fpDict))
 		{
 			fgets(texto, sizeof(texto), fpDict);
+			ltexto = strlen(texto);
 
-			for (cont = 0; cont < strlen(texto); cont++)
+			for (cont = 0; cont < ltexto; cont++)
 			{
-				if (cont > 0 && (texto[cont] == ' ' || texto[cont] == '\t' || texto[cont] == '\n' || texto[cont] == '\r' || texto[cont] == '\0' || texto[cont] == '.' || texto[cont] == ',' || texto[cont] == '(' || texto[cont] == ')' || texto[cont] == ';') && cont > 0 && !esEspacioBlanco(texto[cont - 1]))
+				if (cont > 0 && (texto[cont] == ' ' || texto[cont] == '\t' || texto[cont] == '\n' || texto[cont] == '\r' || texto[cont] == '\0' || texto[cont] == '.' || texto[cont] == ',' || texto[cont] == '(' || texto[cont] == ')' || texto[cont] == ';') && !esEspacioBlanco(texto[cont - 1]))
 				{
 					wdetec[contp] = '\0';
-					_strlwr_s(wdetec);
 					int palabraExistente = 0;
-					for (int i = 0; i < iNumE; i++)
+					for (i = 0; i < iNumE; i++)
 					{
 						if (strcmp(szPalabras[i], wdetec) == 0)
 						{
@@ -77,16 +78,16 @@ void	Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[
 					if (!(texto[cont] == ' ' || texto[cont] == '\t' || texto[cont] == '\n' || texto[cont] == '\r' || texto[cont] == '\0') && texto[cont] != ',' && texto[cont] != '.' && texto[cont] != '(' && texto[cont] != ')' && texto[cont] != ';')
 
 					{
+						texto[cont] = tolower(texto[cont]);
 						wdetec[contp] = texto[cont];
 						contp++;
 					}
 				}
 			}
-			if (cont > 0 && !(texto[strlen(texto) - 1] == ' ' || texto[strlen(texto) - 1] == '\t' || texto[strlen(texto) - 1] == '\n' || texto[strlen(texto) - 1] == '\r' || texto[strlen(texto) - 1] == '\0' || texto[strlen(texto) - 1] == '.'))
-			{
+			if (cont > 0 && (texto[cont] == ' ' || texto[cont] == '\t' || texto[cont] == '\n' || texto[cont] == '\r' || texto[cont] == '\0' || texto[cont] == '.' || texto[cont] == ',' || texto[cont] == '(' || texto[cont] == ')' || texto[cont] == ';') && !esEspacioBlanco(texto[cont - 1])) {
 				wdetec[contp] = '\0';
 				int palabraExistente = 0;
-				for (int i = 0; i < iNumE; i++)
+				for (i = 0; i < iNumE; i++)
 				{
 					if (strcmp(szPalabras[i], wdetec) == 0)
 					{
@@ -108,7 +109,7 @@ void	Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[
 		iNumElementos = iNumE;
 
 		//bubble sort
-		int cont1, cont2, auxi, i;
+		int cont1, cont2, auxi;
 		char aux[TAMTOKEN];
 		for (cont1 = 0; cont1 < iNumElementos - 1; cont1++)
 			for(cont2 = 0; cont2 < iNumElementos - 1; cont2++)
@@ -151,12 +152,32 @@ void	ListaCandidatas		(
 	int		iPeso[],							//Peso de las palabras en la lista final
 	int &	iNumLista)							//Numero de elementos en la szListaFinal
 {
+	iNumLista = 0;
+	int cont, cont2, cont3, band;
 
-	//Sustituya estas lineas por su código
-	strcpy(szListaFinal[0], szPalabrasSugeridas[ 0] ); //la palabra candidata
-	iPeso[0] = iEstadisticas[0];			// el peso de la palabra candidata
-	
-	iNumLista = 1;							//Una sola palabra candidata
+	for (cont = 0; cont < iNumSugeridas; cont++)
+	{
+		for (cont2 = 0; cont2 < iNumElementos; cont2++)
+		{
+			if (strcmp(szPalabrasSugeridas[cont], szPalabras[cont2]) == 0)
+			{
+				band = 0;
+
+				for (cont3 = 0; cont3 < iNumLista && !band; cont3++)
+				{
+					if (strcmp(szListaFinal[cont3], szPalabras[cont2]) == 0)
+						band = 1;
+				}
+				
+				if (!band)
+				{
+					strcpy_s(szListaFinal[iNumLista], szPalabrasSugeridas[cont]);
+					iPeso[iNumLista++] = iEstadisticas[cont2];
+				}
+			}
+		}
+	}
+
 }
 
 /*****************************************************************************************************************
@@ -172,16 +193,17 @@ void	ClonaPalabras(
 {
 
 	char aux2[TAMTOKEN];
-	int cont, cont2, contp = 0;
+	int cont, cont2, contp = 0, npalabra;
 	iNumSugeridas = 0;
-	char dict[38] = "abcdefghijklmnñopqrstuwxyzáéíóú";
+	char dict[39] = "abcdefghijklmnñopqrstuvwxyzáéíóú";
 
 
-	strcpy_s(aux2, szPalabraLeida);
-	
-	for (cont = 0; cont < strlen(szPalabraLeida) && strlen(szPalabraLeida) != 1; cont++)
+	strcpy_s(aux2, TAMTOKEN, szPalabraLeida);
+	npalabra = strlen(szPalabraLeida);
+
+	for (cont = 0; cont < npalabra && npalabra != 1; cont++)
 	{
-		for (cont2 = 0; cont2 < strlen(szPalabraLeida); cont2++)
+		for (cont2 = 0; cont2 < npalabra; cont2++)
 		{
 			if (cont2 != cont)
 			{
@@ -190,45 +212,56 @@ void	ClonaPalabras(
 		}
 		aux2[contp] = '\0';
 
-		strcpy_s(szPalabrasSugeridas[iNumSugeridas++], aux2);
+		strcpy_s(szPalabrasSugeridas[iNumSugeridas++], TAMTOKEN, aux2);
 
-		strcpy_s(aux2, szPalabraLeida);
+		strcpy_s(aux2, TAMTOKEN, szPalabraLeida);
 		contp = 0;
 	}
 
-	for (cont = 0; cont < strlen(szPalabraLeida) - 1; cont++)
+	for (cont = 0; cont < npalabra - 1; cont++)
 	{
 		aux2[cont] = szPalabraLeida[cont + 1];
 		aux2[cont + 1] = szPalabraLeida[cont]; 
 
-		strcpy_s(szPalabrasSugeridas[iNumSugeridas++], aux2); 
-		strcpy_s(aux2, szPalabraLeida); 
+		strcpy_s(szPalabrasSugeridas[iNumSugeridas++], TAMTOKEN, aux2); 
+		strcpy_s(aux2, TAMTOKEN, szPalabraLeida); 
 	}
-	strcpy_s(szPalabrasSugeridas[iNumSugeridas++], szPalabraLeida);
+	strcpy_s(szPalabrasSugeridas[iNumSugeridas++], TAMTOKEN, szPalabraLeida);
 
-	for (cont = 0; cont < strlen(szPalabraLeida); cont++)
+	for (cont = 0; cont < npalabra; cont++)
 	{
 		for (cont2 = 0; cont2 < 32; cont2++)
 		{
 			aux2[cont] = dict[cont2];
-			strcpy_s(szPalabrasSugeridas[iNumSugeridas++], aux2);
+			strcpy_s(szPalabrasSugeridas[iNumSugeridas++], TAMTOKEN, aux2);
 		}
 		aux2[cont] = szPalabraLeida[cont];
 	}
 
-	for (cont = 1; cont < strlen(szPalabraLeida) + 1; cont++)
+	for (cont = 1; cont < npalabra + 1; cont++)
 	{
 		aux2[cont] = szPalabraLeida[cont - 1];
 	}
 	aux2[cont] = '\0';
 
-	for (cont = 0; cont < strlen(szPalabraLeida) + 1; cont++)
+	for (cont = 0; cont < npalabra + 1; cont++)
 	{
 		for (cont2 = 0; cont2 < 32; cont2++)
 		{
 			aux2[cont] = dict[cont2];
-			strcpy_s(szPalabrasSugeridas[iNumSugeridas++], aux2);
+			strcpy_s(szPalabrasSugeridas[iNumSugeridas++], TAMTOKEN, aux2);
 		}
 		aux2[cont] = szPalabraLeida[cont];
 	}
+
+
+	for (cont = 0; cont < iNumSugeridas - 1; cont++) 
+		for (cont2 = 0; cont2 < iNumSugeridas - 1; cont2++) 
+			if (strcmp(szPalabrasSugeridas[cont2], szPalabrasSugeridas[cont2 + 1]) > 0) 
+			{
+				strcpy_s(aux2, TAMTOKEN, szPalabrasSugeridas[cont2]);
+				strcpy_s(szPalabrasSugeridas[cont2], TAMTOKEN, szPalabrasSugeridas[cont2 + 1]);
+				strcpy_s(szPalabrasSugeridas[cont2 + 1], TAMTOKEN, aux2);
+			}
+
 }
